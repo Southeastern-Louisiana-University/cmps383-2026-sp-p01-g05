f87659d57f531f32ca134a679a1bd147abc9a82d
dghall48 <145396671+dghall48@users.noreply.github.com>
2026-01-30 16:50:35 -0600

    Add Entity Framework setup: DataContext, Location entity, LocationDto, and LocationsController with CRUD endpoints

--- Selu383.SP26.Api/Program.cs
+++ Selu383.SP26.Api/Program.cs
@@ -1,13 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Selu383.SP26.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add DbContext
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Auto-migrate database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
